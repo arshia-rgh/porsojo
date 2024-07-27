f72edf8501baa8330890367df3fb30a8e868788a
Ali Bagheri <khodealib@gmail.com>
2024-07-27 20:08:41 +0330

    feat: :sparkles: add user model
    - implement custom user

---
+++ accounts/models/user.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from accounts.validators import iranian_phone_number_validator
+
+
+class User(AbstractUser):
+    """
+    Users within the Django authentication system are represented by this
+    model.
+
+    Username and password are required. Other fields are optional.
+    """
+
+    email = models.EmailField(
+        _("email address"),
+        blank=True,
+        null=True,
+        unique=True,
+    )
+
+    phone_number = models.CharField(
+        _("phone number"),
+        max_length=13,
+        unique=True,
+        null=True,
+        blank=True,
+        validators=[iranian_phone_number_validator],
+    )
+
+    class Meta(AbstractUser.Meta):
+        swappable = "AUTH_USER_MODEL"
