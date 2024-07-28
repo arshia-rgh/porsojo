1301bda5dfb181dc1db5a524010bd683a04be0e2
pxsa <paghsis@gmail.com>
2024-07-28 10:35:34 +0330

    implementing categories model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Folder(models.Model):
+    """
+        The `Folder` model is resposible for the different categories in which user creates.
+
+        user: forign key to custom user.  
+        name: the title or name of the Folder.  
+        description: [optional] brief description of what the `Folder` does.
+    """
+
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='folders')
+    name = models.CharField()
+    description = models.CharField(blank=True)
+
+
+class FolderItem(models.Model):
+    # object_id = 
+    folder = models.ForeignKey(Folder, on_delete=models.PROTECT, related_name='items')
+    content_id = models.IntegerField()
+    
