e13a7371b7050818f83dab53ba6afae9b8a2f66b
arshia <arshiarezagholi1212@gmail.com>
2024-07-29 19:14:07 +0330

    basic implementation

--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Form(models.Model):
+    pass
+
+
+class Question(models.Model):
+    pass
+
+
+class Answer(models.Model):
+    pass
+
+
+class ProcessForm(models.Model):
+    pass
+
+
+class Response(models.Model):
+    pass
+
+
+class Process(models.Model):
+    pass
