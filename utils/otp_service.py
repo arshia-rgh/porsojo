7c0d6fdbe1001cc7f3665cca3cd5284df3260be4
Ali Bagheri <khodealib@gmail.com>
2024-07-29 20:40:21 +0330

    fix: add otp_code_model_class in BaseOtpService

--- utils/otp_service.py
+++ utils/otp_service.py
@@ -6,11 +6,13 @@ from accounts.models.otp_token import OtpToken
 
 
 class BaseOtpService(ABC):
+    otp_code_model_class = OtpToken
+
     def send_otp(self, phone_number: str) -> None:
         pass
 
     def verify_otp(self, phone_number: str, otp: str) -> bool:
-        otp_token = OtpToken.objects.filter(phone_number=phone_number).first()
+        otp_token = self.otp_code_model_class.objects.filter(phone_number=phone_number).first()
         if otp_token and otp_token.code == int(otp) and not otp_token.is_expire:
             otp_token.delete()
             return True
@@ -19,7 +21,7 @@ class BaseOtpService(ABC):
 
 class FakeOtpService(BaseOtpService):
     def send_otp(self, phone_number: str) -> None:
-        otp_token = OtpToken.objects.create(
+        otp_token = self.otp_code_model_class.objects.create(
             phone_number=phone_number, code=OtpToken.generate_code()
         )
         print("--------------------------------")
@@ -32,7 +34,7 @@ class KavenegarOtpService(BaseOtpService):
         self.api = KavenegarAPI(api_key)
 
     def send_otp(self, phone_number: str) -> None:
-        otp_token = OtpToken.objects.create(
+        otp_token = self.otp_code_model_class.objects.create(
             phone_number=phone_number, code=OtpToken.generate_code()
         )
         try:
