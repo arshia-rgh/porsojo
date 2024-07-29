91331f20769f28ff8330fb08e24ce1f28e2619f7
Ali Bagheri <khodealib@gmail.com>
2024-07-29 22:41:25 +0330

    fix: add phone_number fields in VerifyOtpTokenSerializer

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from accounts.models import User
+from accounts.models.otp_token import OtpToken
+from accounts.validators import iranian_phone_number_validator
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
@@ -25,3 +27,33 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class IranianPhoneNumberSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(
+        max_length=13, validators=[iranian_phone_number_validator]
+    )
+
+    @staticmethod
+    def validate_phone_number(value):
+        if not User.objects.filter(phone_number=value).exists():
+            raise serializers.ValidationError(
+                "User with this phone number does not exist."
+            )
+        return value
+
+
+class VerifyOtpTokenSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(
+        max_length=13, validators=[iranian_phone_number_validator]
+    )
+    otp_token = serializers.CharField(max_length=4)
+
+    def validate(self, data):
+        phone_number = data["phone_number"]
+        otp_token = data["otp_token"]
+
+        if not OtpToken.objects.filter(phone_number=phone_number, code=otp_token).exists():
+            raise serializers.ValidationError("Invalid OTP token.")
+
+        return data
