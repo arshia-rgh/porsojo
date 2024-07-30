f645e0bcd01ba6becfe8c3f5dfd347cd50e7d4e5
arshia <arshiarezagholi1212@gmail.com>
2024-07-30 21:24:53 +0330

    added Answer ProcessForm and Response models

--- surveys/models.py
+++ surveys/models.py
@@ -14,9 +14,11 @@ class Form(models.Model):
 
     def save(self, *args, **kwargs):
 
+        # if the form is public set the password to empty string
         if self.is_public:
             self.password = ''
 
+        # if the form is private and the password is empty should raise an exception
         elif not self.is_public and self.password == '':
             raise ValueError("Password cannot be empty for non-public forms")
         super().save(*args, **kwargs)
@@ -31,6 +33,7 @@ class Question(models.Model):
 
 
 class Process(models.Model):
+    forms = models.ManyToManyField(Form, through='ProcessForm')
     title = models.CharField(max_length=100)
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -39,25 +42,35 @@ class Process(models.Model):
     is_linear = models.BooleanField()
 
     def save(self, *args, **kwargs):
+        # If our process contains any private form the process should be private too
+        for form in self.forms.all():
+            if not form.is_public:
+                self.is_public = False
 
+        # if the process is public set the password to empty string
         if self.is_public:
             self.password = ''
 
+        # if the process is private and the password is empty should raise an exception
         elif not self.is_public and self.password == '':
             raise ValueError("Password cannot be empty for non-public processes")
         super().save(*args, **kwargs)
 
 
 class ProcessForm(models.Model):
-    process = models.OneToOneField(Process, on_delete=models.CASCADE)
-    # form id
+    process = models.ForeignKey(Process, on_delete=models.CASCADE)
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    priority_number = models.IntegerField()
 
-# class Answer(models.Model):
-#     pass
-#
-#
 
-# class Response(models.Model):
-#     pass
-#
-#
+class Response(models.Model):
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    Process = models.OneToOneField(Process, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    submitted_at = models.DateTimeField(auto_now=True)
+
+
+class Answer(models.Model):
+    response = models.ForeignKey(Response, on_delete=models.CASCADE)
+    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+    answer_text = models.TextField()
