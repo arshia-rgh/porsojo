4efdda89b216f02ad8d6403b14cb826dbf38e01d
Ali Bagheri <khodealib@gmail.com>
2024-07-30 23:09:48 +0330

    test: VerifyOtpTokenView

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -44,11 +44,19 @@ class VerifyOtpTokenSerializer(serializers.Serializer):
     phone_number = serializers.CharField(max_length=13, validators=[iranian_phone_number_validator])
     otp_token = serializers.CharField(max_length=4)
 
-    def validate(self, data):
-        phone_number = data["phone_number"]
-        otp_token = data["otp_token"]
-
-        if not OtpToken.objects.filter(phone_number=phone_number, code=otp_token).exists():
+    @staticmethod
+    def validate_otp_token(value):
+        if not OtpToken.objects.filter(code=value).exists():
             raise serializers.ValidationError("Invalid OTP token.")
+        return value
 
+    @staticmethod
+    def validate_phone_number(value):
+        if not User.objects.filter(phone_number=value).exists():
+            raise serializers.ValidationError("User with this phone number does not exist.")
+        return value
+
+    def validate(self, data):
+        if not OtpToken.objects.filter(code=data["otp_token"], phone_number=data["phone_number"]).exists():
+            raise serializers.ValidationError("Invalid OTP token.")
         return data
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,8 +1,14 @@
+import json
+from unittest.mock import patch
+
+from django.test import override_settings
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from accounts.models.otp_token import OtpToken
 from accounts.models.user import User
+from utils.otp_service import FakeOtpService
 
 
 class AccountsEndpointTestCase(APITestCase):
@@ -83,3 +89,39 @@ class AccountsEndpointTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.data, {"detail": "No active account found with the given credentials"})
+
+    @override_settings(DEBUG=True)
+    def test_user_send_otp_valid_request(self) -> None:
+        self.create_user()
+        url = reverse("accounts:send_otp_token")
+        response = self.client.post(url, {"phone_number": self.user_data["phone_number"]})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {"message": "OTP sent successfully"})
+
+    @override_settings(DEBUG=False)
+    def test_user_send_otp_invalid_request(self) -> None:
+        url = reverse("accounts:send_otp_token")
+        response = self.client.post(url, {"phone_number": "09127548715"})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue("phone_number" in response.data)
+
+    @override_settings(DEBUG=True)
+    def test_verify_otp_token_valid_request(self):
+        self.create_user()
+        self.client.post(reverse("accounts:send_otp_token"), {"phone_number": self.user_data["phone_number"]})
+
+        url = reverse("accounts:verify_otp_token")
+        otp_code = OtpToken.objects.filter(phone_number=self.user_data["phone_number"]).first().code
+        response = self.client.post(url, {"phone_number": self.user_data["phone_number"], "otp_token": otp_code})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    @override_settings(DEBUG=True)
+    def test_verify_otp_token_invalid_request(self):
+        self.create_user()
+        self.client.post(reverse("accounts:send_otp_token"), {"phone_number": self.user_data["phone_number"]})
+
+        url = reverse("accounts:verify_otp_token")
+        otp_code = OtpToken.objects.filter(phone_number=self.user_data["phone_number"]).first().code
+        response = self.client.post(url, {"phone_number": self.user_data["phone_number"], "otp_token": otp_code + 1})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
