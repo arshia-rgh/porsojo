9fd370d6dc3455a02afb3955d8b534c4eefc77e7
arshia <arshiarezagholi1212@gmail.com>
2024-07-30 20:40:28 +0330

    added process and process form models

--- surveys/models.py
+++ surveys/models.py
@@ -30,17 +30,34 @@ class Question(models.Model):
     options = models.TextField()
 
 
+class Process(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=True)
+    password = models.CharField(max_length=100, blank=True)
+    is_linear = models.BooleanField()
+
+    def save(self, *args, **kwargs):
+
+        if self.is_public:
+            self.password = ''
+
+        elif not self.is_public and self.password == '':
+            raise ValueError("Password cannot be empty for non-public processes")
+        super().save(*args, **kwargs)
+
+
+class ProcessForm(models.Model):
+    process = models.OneToOneField(Process, on_delete=models.CASCADE)
+    # form id
+
 # class Answer(models.Model):
 #     pass
 #
 #
-# class ProcessForm(models.Model):
-#     pass
-#
-#
+
 # class Response(models.Model):
 #     pass
 #
 #
-# class Process(models.Model):
-#     pass
