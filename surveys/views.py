9dda866f4f07f65333cbc7bcd5014bde791c4b25
arshia <arshiarezagholi1212@gmail.com>
2024-08-02 22:17:04 +0330

    changed permission to the IsAuthenticatedOrReadOnly, a anonymous user can send safe requests now

--- surveys/views.py
+++ surveys/views.py
@@ -1,6 +1,6 @@
 from django.core.cache import cache
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from .models import Form
@@ -10,7 +10,7 @@ from .serializers import FormSerializer
 class FormViewSet(viewsets.ModelViewSet):
     queryset = Form.objects.all()
     serializer_class = FormSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     # Cacheing the list method (getting all objects)
     def list(self, request, *args, **kwargs):
