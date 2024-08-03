95f96eeb9d26468e8279bf5a3a29c92e307afa99
pxsa <paghsis@gmail.com>
2024-08-03 10:08:42 +0330

    feat(category): implement serializer for Folder model
    create `FormSerializer` class in serializers.py using ModelSerializer
    
    task #16

---
+++ categories/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Folder
+
+
+class FormSerializer(serializers.ModelSerializer):
+    """
+    serializer class for `Form` model.
+    
+    """
+    class Meta:
+        model = Folder
+        fields = ['name', 'description']
