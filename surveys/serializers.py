fdadd1bbb95d586a3fce71232a0be724b9521dbc
arshia <arshiarezagholi1212@gmail.com>
2024-08-06 18:21:40 +0330

    made user field read only and override the create method

--- surveys/serializers.py
+++ surveys/serializers.py
@@ -7,6 +7,13 @@ class FormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Form
         fields = '__all__'
+        read_only_fields = ['user']
+
+    # set user to the current user
+    def create(self, validated_data):
+        request = self.context.get("request")
+        validated_data['user'] = request.user
+        return super().create(validated_data)
 
 
 class ProcessFormSerializer(serializers.ModelSerializer):
@@ -19,3 +26,10 @@ class ProcessSerializer(serializers.ModelSerializer):
     class Meta:
         model = Process
         fields = "__all__"
+        read_only_fields = ['user']
+
+    # set user to the current user
+    def create(self, validated_data):
+        request = self.context.get("request")
+        validated_data['user'] = request.user
+        return super().create(validated_data)
