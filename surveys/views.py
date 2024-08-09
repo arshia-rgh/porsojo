ac8f42999c00a4870b89ba0eae038470a637551a
arshia <arshiarezagholi1212@gmail.com>
2024-08-09 22:03:05 +0330

    added UserActivityMixin for loggin Form and Process

--- surveys/views.py
+++ surveys/views.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
+from analytics.mixins import UserActivityMixin
 from .mixins import CachedListMixin, ThrottleMixin
 from .models import Form, ProcessForm, Process, Question
 from .serializers import FormSerializer, ProcessFormSerializer, ProcessSerializer, QuestionSerializer
 
 
-class FormViewSet(CachedListMixin, ThrottleMixin, viewsets.ModelViewSet):
+class FormViewSet(CachedListMixin, ThrottleMixin, UserActivityMixin, viewsets.ModelViewSet):
     queryset = Form.objects.all()
     serializer_class = FormSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -19,14 +20,14 @@ class FormViewSet(CachedListMixin, ThrottleMixin, viewsets.ModelViewSet):
         return context
 
 
-class ProcessFormViewSet(CachedListMixin, ThrottleMixin, viewsets.ModelViewSet):
+class ProcessFormViewSet(CachedListMixin, ThrottleMixin, UserActivityMixin, viewsets.ModelViewSet):
     queryset = ProcessForm.objects.all()
     serializer_class = ProcessFormSerializer
     permission_classes = [IsAuthenticated]
     cache_key = "processform_list"
 
 
-class ProcessViewSet(CachedListMixin, ThrottleMixin, viewsets.ModelViewSet):
+class ProcessViewSet(CachedListMixin, ThrottleMixin, UserActivityMixin, viewsets.ModelViewSet):
     queryset = Process.objects.all()
     serializer_class = ProcessSerializer
     permission_classes = [IsAuthenticated]
