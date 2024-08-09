add144dd543811eca6ee06de034026302628cfca
arshia <arshiarezagholi1212@gmail.com>
2024-08-09 21:55:39 +0330

    removed dual super().save() in Procss model

--- surveys/models.py
+++ surveys/models.py
@@ -84,7 +84,6 @@ class Process(models.Model):
         # if the process is private and the password is empty should raise an exception
         elif not self.is_public and self.password == "":
             raise ValueError("Password cannot be empty for non-public processes")
-        super().save(*args, **kwargs)
 
     @property
     def view_count(self) -> int:
