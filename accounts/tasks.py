74ad14ca9cb6d477af50b6e23c8727b15d9aa65e
arshia <arshiarezagholi1212@gmail.com>
2024-08-11 20:14:44 +0330

    basic implement of send verification email celery task

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -3,7 +3,8 @@ from django.conf import settings
 
 from accounts.models.otp_token import OtpToken
 from utils.otp_service import BaseOtpService, FakeOtpService, KavenegarOtpService
-
+from django.core.mail import send_mail
+from utils.email_verification_uid import generate_email_verification_token, generate_uid
 
 def get_otp_service() -> BaseOtpService:
     """
@@ -47,3 +48,13 @@ def delete_otp_token(otp_token_id: int) -> None:
     """
 
     OtpToken.objects.filter(id=otp_token_id).first().delete()
+
+
+@shared_task
+def send_verification_email(user_id, email):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    user = User.objects.get(pk=user_id)
+    token = generate_email_verification_token(user)
+    uid = generate_uid(user)
+
