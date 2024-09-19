8b219b8063a04041be6e34427a0451b0437e95b8
arshia <arshiarezagholi1212@gmail.com>
2024-09-19 18:11:12 +0330

    added testing for send verification email

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,13 +1,15 @@
 import pytest
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from unittest.mock import patch
 
 User = get_user_model()
 
 
 @pytest.mark.django_db
 class TestUserRegister:
-    def test_register_new_user_successfully(self, api_client):
+    @patch("accounts.views.send_verification_email.delay")
+    def test_register_new_user_successfully(self, mock_send_verification_email, api_client):
         response = api_client.post(reverse("accounts:register"),
                                    data={"username": "test", "password": "test pass", "email": "test@gmazil.com"})
 
@@ -15,3 +17,5 @@ class TestUserRegister:
         assert User.objects.get(email="test@gmazil.com") is not None
 
         assert User.objects.get(email="test@gmazil.com").is_email_verified == False
+
+        mock_send_verification_email.assert_called_once_with(User.objects.get(email="test@gmazil.com").id, User.objects.get(email="test@gmazil.com").email)
